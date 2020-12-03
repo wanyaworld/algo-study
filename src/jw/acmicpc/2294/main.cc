2e2297cbdaec1544c0ab4b1ed3d69e510b77d6d2
wanyaworld <6812skiii@gmail.com>
2020-12-04 00:36:43 +0900

    dayum i did it

--- src/jw/acmicpc/2294/main.cc
+++ src/jw/acmicpc/2294/main.cc
@@ -3,47 +3,31 @@
 #include <bits/stdc++.h>
 
 int N_COINS, total;
-int cache[10001][100] = {};
+int cache[10001] = {};
 int coins[100] = {};
 
-int solve(int remaining, int coin_idx) {
-	int old_rem = remaining;
-	int ret;
-	unsigned int total_min = -1;
-	int used = 0;
-
-	if (cache[remaining][coin_idx]) return cache[remaining][coin_idx];
-	if (coin_idx == N_COINS - 1) {
-		if (remaining % coins[coin_idx] == 0)
-			ret = remaining / coins[coin_idx];
-		else
-			ret = -1;
-		
-		goto term;
+int solve(int n) {
+	if (n == 0) return 0;
+	if (n < 0) return -1;
+	if (cache[n]) return cache[n];
+	int min = INT_MAX;
+	for (int i = 0 ; i < N_COINS ; i++) {
+		int tmp = solve(n - coins[i]) + 1;
+		if (tmp == 0) continue;
+		if (tmp < min) min = tmp;
 	}
-	while (1) {
-		ret = solve(remaining, coin_idx + 1);
-		if ((unsigned int)ret + used < total_min && ret != -1) {
-			total_min = ret + used;
-		}
-		remaining -= coins[coin_idx];
-		used++;
-		if (remaining < 0) break;
-	}
-
-	ret = total_min;
-term:
-	cache[old_rem][coin_idx] = ret;
-	return ret;
-
+	if (min == INT_MAX) min = -1;
+	cache[n] = min;
+	return min;
 }
 
 int main() {
 	scanf("%d %d", &N_COINS, &total);
 	for (int i = 0 ; i < N_COINS ; i++)
 		scanf("%d", &(coins[i]));
-	std::sort(coins, coins + N_COINS, std::greater<int>());
-	printf("%d\n", solve(total, 0));
+	//std::sort(coins, coins + N_COINS, std::greater<int>());
+	std::sort(coins, coins + N_COINS);
+	printf("%d\n", solve(total));
 	return 0;
 }
 
