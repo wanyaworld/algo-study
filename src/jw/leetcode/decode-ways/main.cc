62b39a7c8e4976ec6511f18c80108a829920b758
wanyaworld <6812skiii@gmail.com>
2020-12-10 20:25:09 +0900

    asdas

--- src/jw/leetcode/decode-ways/main.cc
+++ src/jw/leetcode/decode-ways/main.cc
@@ -2,48 +2,56 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
-public:
-    string str{};
-    vector<string> vec{};
-    Solution() : cnt(0) {
-        for (char c = 'A' ; c <= 'Z' ; c++) {
-            vec.push_back(to_string(c - 'A' + 1));
-        }
-    }
-    int cnt;
-    vector<string> getStringsStartWith(string s) {
-        vector<string> v;
-        for (auto it = vec.begin() ; it != vec.end() ; it++) {
-            if (!strncmp((char*)it->c_str(), (char*)s.c_str(), it->size()))
-                v.push_back(*it);
-        }
-        return v;
-    }
-    void dfs(std::string str) {
-        if(str.size() == 0) {
-            cnt++;
-            return;
-        }
-        vector<string> v = getStringsStartWith(str);
-        for (auto it = v.begin() ; it != v.end() ; it++) {
-            dfs(str.substr(it->size(), str.size() - 1));
-        }
-        return ;
-    }
-    int numDecodings(string s) {
-        str = s;
-        dfs(str);
-        return cnt;
-    }
+	public:
+		string str{};
+		vector<string> vec{};
+		int N;
+		unordered_map<int, unsigned long long int> cache{};
+		Solution() {
+			for (char c = 'A' ; c <= 'Z' ; c++) {
+				vec.push_back(to_string(c - 'A' + 1));
+			}
+		}
+		vector<string> getStringsStartWith(string s) {
+			vector<string> v;
+			for (auto it = vec.begin() ; it != vec.end() ; it++) {
+				if (!strncmp((char*)it->c_str(), (char*)s.c_str(), s.size()))
+					v.push_back(*it);
+			}
+			return v;
+		}
+		unsigned long long int dfs(int idx) {
+			if (idx >= N) return 0;
+			if (idx == N - 1) {
+				if (str[idx] >= '1' && str[idx] <= '9') return 1;
+				return 0;
+			}
+			if (cache.find(idx) != cache.end()) return cache[idx];	
+			unsigned long long int ret{};
+			ret += dfs(idx + 1);
+			string tmp = str.substr(idx, 2);
+			if (getStringsStartWith(tmp).size() >= 1) {
+				unsigned long long int sub = dfs(idx + 2);
+				if (sub == 0) sub = 1;
+				ret += sub;
+			}
+			cache[idx] = ret;
+			return ret;
+		}
+		unsigned long long int numDecodings(string s) {
+			str = s;
+			N = str.size();
+			return dfs(0);
+		}
 };
 
 int main() {
-  Solution s;
-  s.numDecodings((string)"111111111111111111111111111111");
-  cout << s.cnt << endl;
+	Solution s;
+	cout << s.numDecodings((string)"2101") << endl;
 	return 0;
 }
