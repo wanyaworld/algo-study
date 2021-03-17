22f00df3be9197f262c31540791cf9e65e8bd53b
wanyaworld <6812skiii@gmail.com>
2021-03-17 19:53:39 +0900

    Update main.cpp

--- src/jw/leetcode/200/main.cpp
+++ src/jw/leetcode/200/main.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int dfs(vector<vector<char>>& grid, int y, int x) {
         if (y > m - 1 || y < 0 || x > n - 1 || x < 0) return 0;
-        if (grid[y][x] == 0) return 0;
+        if (grid[y][x] == '0') return 0;
         
-        grid[y][x] = 0;
+        grid[y][x] = '0';
         
         dfs(grid, y - 1, x);
         dfs(grid, y + 1, x);
@@ -17,6 +17,7 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         m = grid.size();
         n = grid[0].size();
+
         for (int i = 0 ; i < m ; i++) {
             for (int j = 0 ; j < n ; j++) {
                 ret += dfs(grid, i, j);
